661210f5351b99410fd1192aaa51534f26e89cca
sai wanza <q020385791@gmail.com>
2021-06-24 08:53:02 +0800

    加入專案檔案。

---
+++ OracleWriter/Program.cs
@@ -0,0 +1,39 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+
+namespace OracleWriter
+{
+    class Program
+    {
+        //using Oracle.ManagedDataAccess.Client
+        public static OracleConnection db = new OracleConnection();
+        static void Main(string[] args)
+        {
+            string sql = "";
+           db.Close();
+            db.ConnectionString = "";
+           db.Open();
+
+            using (OracleTransaction trans = db.BeginTransaction())
+            {
+                OracleCommand command = db.CreateCommand();
+                command.Transaction = trans;
+                command.CommandText = sql;
+                try
+                {
+                    int ChangedCount = command.ExecuteNonQuery();
+                    trans.Commit();
+                }
+                catch (Exception ex)
+                {
+
+                    trans.Rollback();
+                }
+            }
+
+                db.Close();
+        }
+
+        
+    }
+}
